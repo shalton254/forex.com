9e5007f2c1ff9642ff6802b8af6793a95c2c697a
shalton254 <shaltongumba26@gmail.com>
2026-06-11 13:25:06 +0300

    Create User model schema

---
+++ models/User.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  firstName: String,
+  lastName: String,
+  accountBalance: {
+    type: Number,
+    default: 0
+  },
+  totalDeposited: {
+    type: Number,
+    default: 0
+  },
+  totalWithdrawn: {
+    type: Number,
+    default: 0
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  verificationToken: String,
+  phone: String,
+  country: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Method to get user profile without sensitive info
+userSchema.methods.getPublicProfile = function() {
+  const { password, ...profile } = this.toObject();
+  return profile;
+};
+
+module.exports = mongoose.model('User', userSchema);
